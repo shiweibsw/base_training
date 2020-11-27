ad00f03960eb0fb851a56dd9ac02bddc2155726b
shiweibsw <shiweibsw@gmail.com>
2020-11-27 16:19:55 +0800

    28

--- app/src/main/java/com/bsw/base_training/leetcode/leetcode_16.java
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_16.java
@@ -6,6 +6,23 @@ import java.util.Arrays;
 import java.util.Stack;
 
 /**
+ * 给定一个包括 n 个整数的数组 nums 和 一个目标值 target。找出 nums 中的三个整数，使得它们的和与 target 最接近。返回这三个数的和。假定每组输入只存在唯一答案。
+ * <p>
+ *  
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：nums = [-1,2,1,-4], target = 1
+ * 输出：2
+ * 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 3 <= nums.length <= 10^3
+ * -10^3 <= nums[i] <= 10^3
+ * -10^4 <= target <= 10^4
+ *
  * @Author shiwei
  * @Date 2020/11/16-16:04
  * @Email shiweibsw@gmail.com
@@ -20,7 +37,6 @@ public class leetcode_16 {
             while (j < k) {
                 sum = nums[i] + nums[j] + nums[k];
                 if (Math.abs(sum - target) < result) {
-                    Log.i("MainActivity", "i: =" + i + "--j:=" + j + "--k:=" + k + "--sum:=" + sum + "--sum - target:=" + (sum - target));
                     result = Math.abs(sum - target);
                 }
                 if (sum == target) {
