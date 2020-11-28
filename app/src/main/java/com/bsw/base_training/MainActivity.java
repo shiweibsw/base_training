ccbac027746f13a766cea4431f4ec5307357b0f9
shiweibsw <shiweibsw@gmail.com>
2020-11-28 14:21:39 +0800

    28

--- app/src/main/java/com/bsw/base_training/MainActivity.java
+++ app/src/main/java/com/bsw/base_training/MainActivity.java
@@ -1,22 +1,8 @@
 package com.bsw.base_training;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.bsw.base_training.tree.TreeNode;
-
-import java.util.Arrays;
-
-import static com.bsw.base_training.leetcode.leetcode_1572.merge;
-import static com.bsw.base_training.leetcode.leetcode_16.threeSumClosest;
-import static com.bsw.base_training.leetcode.leetcode_167.twoSum;
-import static com.bsw.base_training.leetcode.leetcode_844.backspaceCompare;
-import static com.bsw.base_training.tree.Base.createTree;
-import static com.bsw.base_training.tree.Base.inOrder;
-import static com.bsw.base_training.tree.Base.postOrder;
-import static com.bsw.base_training.tree.Base.preOrder;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -27,8 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViewById(R.id.btn).setOnClickListener(v -> {
-            int[][] a={{1,4},{0,2},{3,5}};
-            merge(a);
+            int[][] a = {{1, 4}, {0, 2}, {3, 5}};
         });
     }
 }
