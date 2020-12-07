ec1ffd15ff2ded39cfff2c575c087832407a0164
shiweibsw <shiweibsw@gmail.com>
2020-12-07 16:31:47 +0800

    23

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_23.java
@@ -0,0 +1,72 @@
+package com.bsw.base_training.leetcode;
+
+/**
+ * 给你一个链表数组，每个链表都已经按升序排列。
+ * <p>
+ * 请你将所有链表合并到一个升序链表中，返回合并后的链表。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：lists = [[1,4,5],[1,3,4],[2,6]]
+ * 输出：[1,1,2,3,4,4,5,6]
+ * 解释：链表数组如下：
+ * [
+ * 1->4->5,
+ * 1->3->4,
+ * 2->6
+ * ]
+ * 将它们合并到一个有序链表中得到。
+ * 1->1->2->3->4->4->5->6
+ * 示例 2：
+ * <p>
+ * 输入：lists = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：lists = [[]]
+ * 输出：[]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * k == lists.length
+ * 0 <= k <= 10^4
+ * 0 <= lists[i].length <= 500
+ * -10^4 <= lists[i][j] <= 10^4
+ * lists[i] 按 升序 排列
+ * lists[i].length 的总和不超过 10^4
+ *
+ * @Author shiwei
+ * @Date 2020/12/7-16:31
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_23 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        ListNode ans = lists[0];
+        for (int i = 1; i < lists.length; i++) {
+            ans = mergeTwoLists(ans, lists[i]);
+        }
+        return ans;
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode solider = new ListNode(0, null);
+        ListNode p = solider;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) p.next = l1;
+        if (l2 != null) p.next = l2;
+        return solider.next;
+    }
+}
