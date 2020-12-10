72ea165ea2818b227f6f5c0d7a1ddc81a3c1bf35
shiweibsw <shiweibsw@gmail.com>
2020-12-10 15:58:44 +0800

    917

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_917.java
@@ -0,0 +1,44 @@
+package com.bsw.base_training.leetcode;
+
+import java.util.Stack;
+
+/**
+ * 给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置发生反转。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入："ab-cd"
+ * 输出："dc-ba"
+ * 示例 2：
+ * <p>
+ * 输入："a-bC-dEf-ghIj"
+ * 输出："j-Ih-gfE-dCba"
+ * 示例 3：
+ * <p>
+ * 输入："Test1ng-Leet=code-Q!"
+ * 输出："Qedo1ct-eeLg=ntse-T!"
+ *
+ * @Author shiwei
+ * @Date 2020/12/10-15:47
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_917 {
+    public String reverseOnlyLetters(String S) {
+        Stack<Character> letters = new Stack();
+        for (char c : S.toCharArray())
+            if (Character.isLetter(c))
+                letters.push(c);
+
+        StringBuilder ans = new StringBuilder();
+        for (char c : S.toCharArray()) {
+            if (Character.isLetter(c))
+                ans.append(letters.pop());
+            else
+                ans.append(c);
+        }
+
+        return ans.toString();
+    }
+}
