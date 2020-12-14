5c3e06ad97e8f8cda2372f3739444fe41a9334f7
shiweibsw <shiweibsw@gmail.com>
2020-12-14 19:29:19 +0800

    696

--- app/src/main/java/com/bsw/base_training/leetcode/leetcode_415.java
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_415.java
@@ -12,7 +12,6 @@ class leetcode_415 {
         int di = 0;
         int i = num1.length() - 1;
         int j = num2.length() - 1;
-        Stack<Integer> stack = new Stack();
         StringBuilder sb = new StringBuilder();
         while (i >= 0 || j >= 0 || di == 1) {
             int s1 = i >= 0 ? num1.charAt(i--) - '0' : 0;
