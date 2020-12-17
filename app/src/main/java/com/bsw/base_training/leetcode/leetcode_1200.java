8c713afacb2ed32a9f2487c6ecdf4d65fbf6f368
shiweibsw <shiweibsw@gmail.com>
2020-12-17 15:31:00 +0800

    1200

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_1200.java
@@ -0,0 +1,55 @@
+package com.bsw.base_training.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给你个整数数组 arr，其中每个元素都 不相同。
+ * <p>
+ * 请你找到所有具有最小绝对差的元素对，并且按升序的顺序返回。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：arr = [4,2,1,3]
+ * 输出：[[1,2],[2,3],[3,4]]
+ * 示例 2：
+ * <p>
+ * 输入：arr = [1,3,6,10,15]
+ * 输出：[[1,3]]
+ * 示例 3：
+ * <p>
+ * 输入：arr = [3,8,-10,23,19,-4,-14,27]
+ * 输出：[[-14,-10],[19,23],[23,27]]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 2 <= arr.length <= 10^5
+ * -10^6 <= arr[i] <= 10^6
+ *
+ * @Author shiwei
+ * @Date 2020/12/17-15:30
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_1200 {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length - 1; i++) {
+            min = Math.min(min, arr[i + 1] - arr[i]);
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] == min) {
+                List<Integer> l1 = new ArrayList<Integer>();
+                l1.add(arr[i]);
+                l1.add(arr[i + 1]);
+                res.add(l1);
+            }
+        }
+        return res;
+    }
+}
