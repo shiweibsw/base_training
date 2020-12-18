c4d1d84d6e81dc9ac505620efded1881d7418ff2
shiweibsw <shiweibsw@gmail.com>
2020-12-18 10:26:52 +0800

    389

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_389.java
@@ -0,0 +1,63 @@
+package com.bsw.base_training.leetcode;
+
+
+/**
+ * 给定两个字符串 s 和 t，它们只包含小写字母。
+ * <p>
+ * 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+ * <p>
+ * 请找出在 t 中被添加的字母。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "abcd", t = "abcde"
+ * 输出："e"
+ * 解释：'e' 是那个被添加的字母。
+ * 示例 2：
+ * <p>
+ * 输入：s = "", t = "y"
+ * 输出："y"
+ * 示例 3：
+ * <p>
+ * 输入：s = "a", t = "aa"
+ * 输出："a"
+ * 示例 4：
+ * <p>
+ * 输入：s = "ae", t = "aea"
+ * 输出："a"
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 0 <= s.length <= 1000
+ * t.length == s.length + 1
+ * s 和 t 只包含小写字母
+ *
+ * @Author shiwei
+ * @Date 2020/12/18-9:21
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_389 {
+    public char findTheDifference(String s, String t) {
+
+        int ret = 0;
+        for (int i = 0; i < s.length(); i++) {
+            ret ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            ret ^= t.charAt(i);
+        }
+        return (char) ret;
+
+
+        // int s1=0;
+        // int i=0;
+        // while(i<s.length()){
+        //     s1+=t.charAt(i)-s.charAt(i);
+        //     i++;
+        // }
+        // return (char)(s1+t.charAt(t.length()-1));
+    }
+}
