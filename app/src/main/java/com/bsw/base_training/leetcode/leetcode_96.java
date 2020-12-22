3964f1710d2bea7794ad572db94e470eb09e4aba
shiweibsw <shiweibsw@gmail.com>
2020-12-22 15:24:24 +0800

    96

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_96.java
@@ -0,0 +1,35 @@
+package com.bsw.base_training.leetcode;
+
+/**
+ * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 3
+ * 输出: 5
+ * 解释:
+ * 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
+ *
+ * @Author shiwei
+ * @Date 2020/12/22-15:23
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_96 {
+    /**
+     * 卡塔兰数
+     */
+    public int numTrees(int n) {
+        // 提示：我们在这里需要用 long 类型防止计算过程中的溢出
+        long C = 1;
+        for (int i = 0; i < n; ++i) {
+            C = C * 2 * (2 * i + 1) / (i + 2);
+        }
+        return (int) C;
+    }
+}
