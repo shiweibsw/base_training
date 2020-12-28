f5814faf3b4f9e25b5ada25dfba9fcdd3111ffaa
shiweibsw <shiweibsw@gmail.com>
2020-12-28 17:20:04 +0800

    94

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_94.java
@@ -0,0 +1,81 @@
+package com.bsw.base_training.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 示例 2：
+ * <p>
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：root = [1]
+ * 输出：[1]
+ * 示例 4：
+ * <p>
+ * <p>
+ * 输入：root = [1,2]
+ * 输出：[2,1]
+ * 示例 5：
+ * <p>
+ * <p>
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 <= Node.val <= 100
+ *  
+ * <p>
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ * @Author shiwei
+ * @Date 2020/12/28-17:18
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_94 {
+    List<Integer> res = new ArrayList();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inOrder(root);
+        return res;
+    }
+
+    private void inOrder(TreeNode root) {
+        if (root == null) return;
+        inOrder(root.left);
+        res.add(root.val);
+        inOrder(root.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
