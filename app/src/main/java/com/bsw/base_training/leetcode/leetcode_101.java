f753574352d1b84dd0cf30d5085ada77de7ab20d
shiweibsw <shiweibsw@gmail.com>
2020-12-28 14:36:21 +0800

    101

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_101.java
@@ -0,0 +1,60 @@
+package com.bsw.base_training.leetcode;
+
+/**
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * <p>
+ *  
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ *  
+ * <p>
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ *  
+ * <p>
+ * 进阶：
+ * <p>
+ * 你可以运用递归和迭代两种方法解决这个问题吗？
+ *
+ * @Author shiwei
+ * @Date 2020/12/28-14:07
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_101 {
+
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    private boolean check(TreeNode r1, TreeNode r2) {
+        if (r1 == null && r2 == null) {
+            return true;
+        }
+        if (r1 == null || r2 == null) {
+            return false;
+        }
+        return r1.val == r2.val && check(r1.left, r2.right) && check(r1.right, r2.left);
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
