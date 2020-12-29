4aafdb74ef419a8ebc24fa77aa1155df7e7e6de5
shiweibsw <shiweibsw@gmail.com>
2020-12-29 17:28:10 +0800

    290

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_290.java
@@ -0,0 +1,66 @@
+package com.bsw.base_training.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
+ * <p>
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入: pattern = "abba", str = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:pattern = "abba", str = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: pattern = "aaaa", str = "dog cat cat dog"
+ * 输出: false
+ * 示例 4:
+ * <p>
+ * 输入: pattern = "abba", str = "dog dog dog dog"
+ * 输出: false
+ * 说明:
+ * 你可以假设 pattern 只包含小写字母， str 包含了由单个空格分隔的小写字母。
+ *
+ * @Author shiwei
+ * @Date 2020/12/29-17:27
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_290 {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        //字符和单词是互相映射，数量必须相等
+        if (words.length != pattern.length()) {
+            return false;
+        }
+        Map<Object, Integer> map = new HashMap();
+        for (Integer i = 0; i < words.length; i++) {
+            /*
+                如果key不存在，插入成功，返回null；如果key存在，返回之前对应的value。
+
+                以pattern = "abba", str = "dog cat cat dog"为例，
+                第1次：map.put('a',0)返回null，map.put("dog",0)返回null，两者相等；
+                第2次：map.put('b',1)返回null，map.put("cat",1)返回null，两者相等；
+                第3次：map.put('b',2)返回1，map.put("cat",2)返回1，两者相等；
+                第4次：map.put('a',3)返回0，map.put("dog",3)返回0，两者相等，
+                结果为 true。
+
+                以pattern = "abba", str = "dog cat cat fish"为例，
+                第1次：map.put('a',0)返回null，map.put("dog",0)返回null，两者相等；
+                第2次：map.put('b',1)返回null，map.put("cat",1)返回null，两者相等；
+                第3次：map.put('b',2)返回1，map.put("cat",2)返回1，两者相等；
+                第4次：map.put('a',3)返回0，map.put("fish",3)返回null，两者不相等，
+                结果为 false。
+            */
+            if (map.put(pattern.charAt(i), i) != map.put(words[i], i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
