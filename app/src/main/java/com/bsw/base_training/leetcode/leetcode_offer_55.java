39bcb06ce8d17db6370a3e5d2594f216e17bb9a6
shiweibsw <shiweibsw@gmail.com>
2020-12-31 10:14:17 +0800

    offer 55

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_offer_55.java
@@ -0,0 +1,44 @@
+package com.bsw.base_training.leetcode;
+
+/**
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ * <p>
+ * 例如：
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回它的最大深度 3 。
+ * <p>
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 节点总数 <= 10000
+ *
+ * @Author shiwei
+ * @Date 2020/12/31-10:13
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_offer_55 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
