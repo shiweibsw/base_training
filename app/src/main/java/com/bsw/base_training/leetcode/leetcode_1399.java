9635406ee45448221d4e24ee5feb60637e0b022a
shiweibsw <shiweibsw@gmail.com>
2021-01-26 15:15:21 +0800

    1399

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_1399.java
@@ -0,0 +1,59 @@
+package com.bsw.base_training.leetcode;
+
+/**
+ * @Author shiwei
+ * @Date 2021/1/26-15:14
+ * @Email shiweibsw@gmail.com
+ * 给你一个整数 n 。请你先求出从 1 到 n 的每个整数 10 进制表示下的数位和（每一位上的数字相加），然后把数位和相等的数字放到同一个组中。
+ * <p>
+ * 请你统计每个组中的数字数目，并返回数字数目并列最多的组有多少个。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 13
+ * 输出：4
+ * 解释：总共有 9 个组，将 1 到 13 按数位求和后这些组分别是：
+ * [1,10]，[2,11]，[3,12]，[4,13]，[5]，[6]，[7]，[8]，[9]。总共有 4 个组拥有的数字并列最多。
+ * 示例 2：
+ * <p>
+ * 输入：n = 2
+ * 输出：2
+ * 解释：总共有 2 个大小为 1 的组 [1]，[2]。
+ * 示例 3：
+ * <p>
+ * 输入：n = 15
+ * 输出：6
+ * 示例 4：
+ * <p>
+ * 输入：n = 24
+ * 输出：5
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 10^4
+ */
+class leetcode_1399 {
+    public int countLargestGroup(int n) {
+        int ans = 0;
+        int max = 0;
+        int[] arr = new int[37];
+        for (int i = 1; i <= n; i++) {
+            int t = i;
+            int sum = 0;
+            while (t != 0) {
+                sum += t % 10;
+                t /= 10;
+            }
+            max = Math.max(max, ++arr[sum]);
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == max) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
