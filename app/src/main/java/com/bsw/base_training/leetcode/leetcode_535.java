ed9fba9da149539ca12cefb5db2364eb7f3b31bc
shiweibsw <shiweibsw@gmail.com>
2021-01-27 13:31:15 +0800

    535

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_535.java
@@ -0,0 +1,42 @@
+package com.bsw.base_training.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * @Author shiwei
+ * @Date 2021/1/27-13:30
+ * @Email shiweibsw@gmail.com
+ * TinyURL是一种URL简化服务， 比如：当你输入一个URL https://leetcode.com/problems/design-tinyurl 时，它将返回一个简化的URL http://tinyurl.com/4e9iAk.
+ * <p>
+ * 要求：设计一个 TinyURL 的加密 encode 和解密 decode 的方法。你的加密和解密算法如何设计和运作是没有限制的，你只需要保证一个URL可以被加密成一个TinyURL，并且这个TinyURL可以用解密方法恢复成原本的URL。
+ */
+class leetcode_535 {
+    String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    Map<String, String> map = new HashMap();
+    Random rand = new Random();
+    String key = getRand();
+
+    private String getRand() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            sb.append(alphabet.charAt(rand.nextInt(62)));
+        }
+        return sb.toString();
+    }
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        while (map.containsKey(key)) {
+            key = getRand();
+        }
+        map.put(key, longUrl);
+        return "http://tinyurl.com/" + key;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(shortUrl.replace("http://tinyurl.com/", ""));
+    }
+}
