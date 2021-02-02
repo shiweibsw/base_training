8f7aee52d3bfb6471995d42dbdb145e785705c1c
shiweibsw <shiweibsw@gmail.com>
2021-02-02 17:16:31 +0800

    offer 09

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_offer_09.java
@@ -0,0 +1,38 @@
+package com.bsw.base_training.leetcode;
+
+import java.util.Stack;
+
+/**
+ * @Author shiwei
+ * @Date 2021/2/2-17:14
+ * @Email shiweibsw@gmail.com
+ */
+class leetcode_offer_09 {
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
+
+    public leetcode_offer_09() {
+        s1 = new Stack<Integer>();
+        s2 = new Stack<Integer>();
+    }
+
+    public void appendTail(int value) {
+        s1.push(value);
+    }
+
+    public int deleteHead() {
+        if (!s2.isEmpty()) {
+            return s2.pop();
+        } else {
+            if (s1.isEmpty()) {
+                return -1;
+            } else {
+                while (!s1.isEmpty()) {
+                    s2.push(s1.pop());
+                }
+                return s2.pop();
+            }
+        }
+
+    }
+}
