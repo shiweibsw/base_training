d806115cffb45d53325aba3a44616d5d87857b5b
shiweibsw <shiweibsw@gmail.com>
2021-03-22 15:25:08 +0800

    73

---
+++ app/src/main/java/com/bsw/base_training/leetcode/leetcode_73.java
@@ -0,0 +1,105 @@
+package com.bsw.base_training.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author shiwei
+ * @Date 2021/3/22-14:58
+ * @Email shiweibsw@gmail.com
+ * 给定一个 m x n 的矩阵，如果一个元素为 0 ，则将其所在行和列的所有元素都设为 0 。请使用 原地 算法。
+ * <p>
+ * 进阶：
+ * <p>
+ * 一个直观的解决方案是使用  O(mn) 的额外空间，但这并不是一个好的解决方案。
+ * 一个简单的改进方案是使用 O(m + n) 的额外空间，但这仍然不是最好的解决方案。
+ * 你能想出一个仅使用常量空间的解决方案吗？
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,1,1],[1,0,1],[1,1,1]]
+ * 输出：[[1,0,1],[0,0,0],[1,0,1]]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+ * 输出：[[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * m == matrix.length
+ * n == matrix[0].length
+ * 1 <= m, n <= 200
+ * -231 <= matrix[i][j] <= 231 - 1
+ */
+class leetcode_73 {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean row0 = false;
+        boolean col0 = false;
+        for (int i = 0; i < n; i++) {
+            if (matrix[0][i] == 0) {
+                row0 = true;
+                break;
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                col0 = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (row0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+//        int m = matrix.length;
+//        int n = matrix[0].length;
+//        Set<Integer> row = new HashSet<Integer>();
+//        Set<Integer> col = new HashSet<Integer>();
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (matrix[i][j] == 0) {
+//                    row.add(i);
+//                    col.add(j);
+//                }
+//            }
+//        }
+//        for (int i : row) {
+//            for (int j = 0; j < n; j++) {
+//                matrix[i][j] = 0;
+//            }
+//        }
+//        for (int j : col) {
+//            for (int i = 0; i < m; j++) {
+//                matrix[i][j] = 0;
+//            }
+//        }
+    }
+}
